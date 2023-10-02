fb607fea155c6eeec97cb8234257fbafd6ce0912
xrchitron <yuahochen1124@gmail.com>
2023-10-02 17:21:57 +0800

    added travis file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed!
+          learn react
         </a>
       </header>
     </div>
